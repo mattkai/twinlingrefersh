5238ff6a719727b20dda6ed6aaf367457e0bc81c
kaikai <827221147@qq.com>
2017-06-30 14:08:16 +0800

    twinlingRefresh 上拉下拉刷新依赖库

---
+++ app/src/main/java/com/twnrm/wk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.twnrm.wk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ twinklingrefresh/src/main/java/com/twinklingrefresh/wllk/PeRefreshLayout/PeRefreshLayoutListener.java
@@ -0,0 +1,11 @@
+package com.twinklingrefresh.wllk.PeRefreshLayout;
+
+/**
+ * Created by Administrator on 2017/3/28.
+ */
+
+public interface PeRefreshLayoutListener {
+
+    public void onPeRefreshLayoutClick();
+
+}
